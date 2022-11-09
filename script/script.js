64f5928b5aa41eb534b577e8edfcd1546862e2a1
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-11-09 18:36:12 -0300

    Corrigido path Menu hb

--- script/script.js
+++ script/script.js
@@ -5,13 +5,13 @@ function toogleMenu() {
   const nav = document.querySelector(".nav")
   nav.classList.toggle("active")
   if (nav.classList.contains("active")) {
-    menuhb.style.backgroundImage = "url('../images/icon-close.svg')";
+    menuhb.style.backgroundImage = "url('/images/icon-close.svg')";
     menuhb.style.transform = "rotate(180deg)"
     menuhb.style.width = "20px"
     document.body.style.overflow = "hidden"
   }
   else {
-    menuhb.style.backgroundImage = "url('../images/icon-hamburger.svg')";
+    menuhb.style.backgroundImage = "url('/images/icon-hamburger.svg')";
     menuhb.style.transform = "rotate(360deg)"
     menuhb.style.width = "24px"
     document.body.style.overflow = "visible"
